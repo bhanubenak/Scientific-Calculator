a619f322ba1b785a63a023a6e20b64967490aed9
bhanubenak <58963477+bhanubenak@users.noreply.github.com>
2023-04-12 14:14:05 +0000

    updating code

---
+++ src/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const display = document.getElementById('calc-display');
+    // console.log(display);
+    const buttons = document.getElementsByClassName('btn');
+    // console.log(buttons)
+
+    let curr_val = "";
+
+    function evaluateResult() {
+        const convertedVal = curr_val
+                .replace("×","*")
+                .replace("÷","/")
+                .replace("%","*0.01")
+
+        const result = eval(convertedVal);
+        curr_val = result.toString();
+        display.value = curr_val;
+    }
+
+
+
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        button.addEventListener('click', function () {
+            // console.log('button clicked', button.innerHTML);
+            const value = button.innerHTML;
+
+            if (value == "AC") {
+                curr_val = "";
+                display.value = curr_val;
+
+            } else if (value == "=") {
+                evaluateResult()
+            } else {
+                curr_val += value;
+                display.value = curr_val;
+            }
+        })
+    }
+})
