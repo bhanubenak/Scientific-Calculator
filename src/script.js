043ae0a025d532062ffe69e593899ba7fd20b03f
bhanubenak <58963477+bhanubenak@users.noreply.github.com>
2023-04-16 05:06:38 +0000

    fx-button

--- src/script.js
+++ src/script.js
@@ -2,6 +2,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const display = document.getElementById('calc-display');
     // console.log(display);
     const buttons = document.getElementsByClassName('btn');
+    // const Fx_btn = document.querySelector('button:contains("Fx")');
+    // console.log(Fx_btn)
+    // const basic_btn = document.querySelector('button:contains("123")');
+    // console.log(basic_btn)
+
     // console.log(buttons)
 
     let curr_val = "";
@@ -22,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for (let i = 0; i < buttons.length; i++) {
         const button = buttons[i];
         button.addEventListener('click', function () {
-            // console.log('button clicked', button.innerHTML);
+            console.log('button clicked', button.innerHTML);
             const value = button.innerHTML;
 
             if (value == "AC") {
